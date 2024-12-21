eb3ac3fc2b5d6eb3805d0c95d3c84374bc5cf903
Maksim Zaikin <Max.V.Zaikin@gmail.com>
2024-12-21 20:01:51 +1100

    fix: .gitignore updated

--- ALARM-System/Alarm/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Alarm.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
